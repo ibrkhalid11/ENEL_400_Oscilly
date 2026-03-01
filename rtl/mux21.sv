81f6fb7922e52523e700e7bbed57f2d43d76d12e
ibrkhalid11 <157555170+ibrkhalid11@users.noreply.github.com>
2026-02-28 22:47:15 -0700

    Unverified DC path with SAR. going to test this rtl in real life tomorrow. it lints and a bitstream was generated.

---
+++ rtl/mux21.sv
@@ -0,0 +1,19 @@
+module mux21
+    #(
+        parameter WIDTH = 1
+    )
+    (
+        input logic select,
+        input logic [WIDTH - 1 : 0] d0,
+        input logic [WIDTH - 1 : 0] d1,
+        output logic [WIDTH - 1 : 0] y
+    );
+    always_comb begin
+        case (select) 
+        1'b0 : y = d0;
+        1'b1 : y = d1;
+        default : y = d0;
+        endcase         
+    end
+
+endmodule
